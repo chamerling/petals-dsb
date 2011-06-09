7da2be3465f1e96e3bc7e78af7529169e2746531
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-06-09 13:19:46 +0000

    Update to COMMONS-38: Add cronexpression validation http://jira.petalslink.com/browse/COMMONS-38
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@25413 7b29199d-e5f1-46e2-806f-aa95afc057b2

--- modules/dsb-kernel-servicepoller/src/main/java/org/petalslink/dsb/kernel/servicepoller/ServicePollerServiceImpl.java
+++ modules/dsb-kernel-servicepoller/src/main/java/org/petalslink/dsb/kernel/servicepoller/ServicePollerServiceImpl.java
@@ -3,12 +3,18 @@
  */
 package org.petalslink.dsb.kernel.servicepoller;
 
+import java.text.ParseException;
+
 import org.objectweb.fractal.fraclet.annotation.annotations.FractalComponent;
 import org.objectweb.fractal.fraclet.annotation.annotations.Interface;
 import org.objectweb.fractal.fraclet.annotation.annotations.LifeCycle;
+import org.objectweb.fractal.fraclet.annotation.annotations.Monolog;
 import org.objectweb.fractal.fraclet.annotation.annotations.Provides;
 import org.objectweb.fractal.fraclet.annotation.annotations.Requires;
 import org.objectweb.fractal.fraclet.annotation.annotations.type.LifeCycleType;
+import org.objectweb.util.monolog.api.Logger;
+import org.ow2.petals.util.LoggingUtil;
+import org.petalslink.dsb.servicepoller.api.CronExpressionValidator;
 import org.petalslink.dsb.servicepoller.api.DocumentHandler;
 import org.petalslink.dsb.servicepoller.api.ServicePoller;
 import org.petalslink.dsb.servicepoller.api.ServicePollerException;
@@ -30,15 +36,21 @@ public class ServicePollerServiceImpl implements ServicePollerService {
 
     @Requires(name = "servicepoller-manager", signature = ServicePoller.class)
     private ServicePoller manager;
+    
+    @Monolog(name = "logger")
+    private Logger logger;
+
+    private LoggingUtil log;
 
     @LifeCycle(on = LifeCycleType.START)
     protected void start() {
-        System.out.println("######----->START SERVICEPOLLERSERVICE");
+        this.log = new LoggingUtil(logger);
+        log.start();
     }
 
     @LifeCycle(on = LifeCycleType.STOP)
     protected void stop() {
-        System.out.println("######----->STOP SERVICEPOLLERSERVICE");
+        log.start();
     }
 
     /*
@@ -51,6 +63,11 @@ public class ServicePollerServiceImpl implements ServicePollerService {
      */
     public void start(ServicePollerInformation toPoll, DocumentHandler inputMessage,
             String cronExpression, ServicePollerInformation replyTo) throws ServicePollerException {
+        try {
+            CronExpressionValidator.validateExpression(cronExpression);
+        } catch (ParseException e) {
+            throw new ServicePollerException(String.format("Invalid CronExpression %s", cronExpression), e);
+        }
         this.getAdapter().start(toPoll, inputMessage, cronExpression, replyTo);
     }
 
