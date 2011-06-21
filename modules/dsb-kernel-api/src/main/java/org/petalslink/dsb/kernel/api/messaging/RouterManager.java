ae57e3e335b8431824433624fb81eb3383b209a3
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-06-21 12:53:18 +0000

    Future work on ESB abstraction
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@25601 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-kernel-api/src/main/java/org/petalslink/dsb/kernel/api/messaging/RouterManager.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.kernel.api.messaging;
+
+/**
+ * Manages {@link RouterModule}s.
+ * 
+ * @author chamerling
+ *
+ */
+public interface RouterManager {
+    
+    /**
+     * Get a module based on its name
+     * 
+     * @param name
+     * @return
+     */
+    RouterModule getModule(String name);
+    
+    /**
+     * Add a module to the modules collection
+     * 
+     * @param module
+     */
+    void addModule(RouterModule module);
+    
+    /**
+     * Removes a module from the modules collection
+     * 
+     * @param name
+     * @return
+     */
+    RouterModule deleteModule(String name);
+
+}
---
+++ modules/dsb-kernel-api/src/main/java/org/petalslink/dsb/kernel/api/messaging/RouterModule.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.kernel.api.messaging;
+
+import org.petalslink.dsb.api.MessageExchange;
+
+/**
+ * A router module. Everything can be done in such module since modules are
+ * linked together in a linked list to provide a very extensible routing
+ * mechanism.
+ * 
+ * @author chamerling
+ * 
+ */
+public interface RouterModule {
+
+    /**
+     * 
+     * @param message
+     */
+    void route(MessageExchange message);
+
+    /**
+     * A unique name in the modules collection
+     * 
+     * @return
+     */
+    String getName();
+
+}
