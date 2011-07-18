9bb490352d4c3f88fef894b879900a73b86acd74
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-07-18 10:33:42 +0000

    ADD TYPE TO REPORT
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@26067 7b29199d-e5f1-46e2-806f-aa95afc057b2

--- modules/dsb-monitoring-api/src/main/java/org/petalslink/dsb/monitoring/api/ReportBean.java
+++ modules/dsb-monitoring-api/src/main/java/org/petalslink/dsb/monitoring/api/ReportBean.java
@@ -242,4 +242,18 @@ public class ReportBean {
         return builder.toString();
     }
 
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
 }
