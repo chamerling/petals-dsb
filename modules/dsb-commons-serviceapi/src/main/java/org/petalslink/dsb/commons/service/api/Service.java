24f686fc1f435b7e4f59d1a9e2d73c178f23e81b
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-08-12 14:29:50 +0000

    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@26581 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-commons-serviceapi/src/main/java/org/petalslink/dsb/commons/service/api/Service.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.commons.service.api;
+
+/**
+ * @author chamerling
+ *
+ */
+public interface Service {
+    
+    void start();
+    
+    void stop();
+}
