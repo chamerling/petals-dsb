4cc44469e19d25d7db21bb115f4cb31c913b7f37
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-08-24 15:27:26 +0000

    Add client/server sample for WSN with the DSB
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@27204 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ samples/dsb-wsn-sample/src/test/java/org/petalslink/dsb/sample/wsn/MainTest.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.sample.wsn;
+
+import junit.framework.TestCase;
+
+/**
+ * @author chamerling
+ *
+ */
+public class MainTest extends TestCase {
+    
+    public void testMain() throws Exception {
+        Main.main(null);
+    }
+
+}
