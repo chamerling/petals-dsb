e77c2bf7cc3c2885165bb202316195aa5816d8af
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-09-21 15:30:57 +0000

    Fix topic retrieval
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@28529 7b29199d-e5f1-46e2-806f-aa95afc057b2

--- modules/dsb-notification-httpclient/src/main/java/org/petalslink/dsb/notification/client/http/simple/HTTPProducerRPClient.java
+++ modules/dsb-notification-httpclient/src/main/java/org/petalslink/dsb/notification/client/http/simple/HTTPProducerRPClient.java
@@ -14,6 +14,7 @@ import org.petalslink.dsb.notification.commons.api.client.simple.RPClient;
 import org.w3c.dom.Element;
 
 import com.ebmwebsourcing.wsstar.topics.datatypes.api.WstopConstants;
+import com.ebmwebsourcing.wsstar.topics.datatypes.api.refinedabstraction.RefinedWstopFactory;
 
 /**
  * @author chamerling
@@ -30,7 +31,7 @@ public class HTTPProducerRPClient implements RPClient {
      */
     public HTTPProducerRPClient(String endpoint) {
         this.endpoint = endpoint;
-        this.client = new HTTPNotificationProducerRPClient(endpoint);
+        this.client = new HTTPNotificationProducerRPClient(this.endpoint);
     }
 
     /*
@@ -47,12 +48,25 @@ public class HTTPProducerRPClient implements RPClient {
                     .getResourceProperty(WstopConstants.TOPIC_SET_QNAME);
             List<Element> elements = response.getPropertyValue();
             for (Element element : elements) {
-                System.out.println(element);
+                if (element.getNodeName().contains("TopicSet")) {
+                    com.ebmwebsourcing.wsstar.topics.datatypes.api.abstraction.TopicSetType topicSet = RefinedWstopFactory
+                            .getInstance().getWstopReader()
+                            .readTopicSetType(element.getOwnerDocument());
+                    for (Element e : topicSet.getTopicsTrees()) {
+                        String nodeName = e.getNodeName();
+                        String prefix = null;
+                        if (nodeName.contains(":")) {
+                            prefix = nodeName.substring(0, nodeName.lastIndexOf(':'));
+                            nodeName = nodeName.substring(nodeName.lastIndexOf(':') + 1);
+                        }
+                        String ns = e.getAttribute("xmlns:"+prefix);
+                        result.add(new QName(ns, nodeName, prefix));
+                    }
+                }
             }
         } catch (Exception e) {
             throw new NotificationException(e);
         }
         return result;
     }
-
 }
