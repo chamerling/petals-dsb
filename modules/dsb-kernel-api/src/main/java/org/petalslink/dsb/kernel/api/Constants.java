cbf2a6a3c892bf816786ff6e20b522be680be339
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-09-26 20:25:28 +0000

    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@28597 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-kernel-api/src/main/java/org/petalslink/dsb/kernel/api/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.kernel.api;
+
+/**
+ * @author chamerling
+ *
+ */
+public interface Constants {
+    
+    String MESSAGE_SKIP_MONITORING = "org.petalslink.dsb.monitoring.skip";
+
+}
