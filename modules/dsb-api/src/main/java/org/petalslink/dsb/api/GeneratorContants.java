c289ebd64cd85dfbea11dec81e91fb783267a1b6
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-09-29 21:24:12 +0000

    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@28758 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-api/src/main/java/org/petalslink/dsb/api/GeneratorContants.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.api;
+
+/**
+ * @author chamerling
+ *
+ */
+public interface GeneratorContants {
+    
+    static final String ENDPOINT_NAME = "GeneratorEndpoint";
+
+    static final String INTERFACE_NAME = "GeneratorInterface";
+
+    static final String SERVICE_NAME = "GeneratorService";
+
+    /**
+     * NS Template where %s is the component name or the protocol...
+     */
+    static final String NS_TEMPLATE = "http://petals.ow2.org/generator/%s/";
+
+}
