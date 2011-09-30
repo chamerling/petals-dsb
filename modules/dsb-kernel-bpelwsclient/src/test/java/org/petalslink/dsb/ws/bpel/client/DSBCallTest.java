4bbb068e176a626bf772ca7f83e834cd399c741c
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-09-30 10:05:14 +0000

    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@28770 7b29199d-e5f1-46e2-806f-aa95afc057b2

--- modules/dsb-kernel-bpelwsclient/src/test/java/org/petalslink/dsb/ws/bpel/client/DSBCallTest.java
+++ modules/dsb-kernel-bpelwsclient/src/test/java/org/petalslink/dsb/ws/bpel/client/DSBCallTest.java
@@ -4,9 +4,9 @@
 package org.petalslink.dsb.ws.bpel.client;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.petalslink.dsb.ws.api.DSBWebServiceException;
 
@@ -35,17 +35,26 @@ public class DSBCallTest {
         }
 
         File bpel = new File(folder, bpelFileName);
-        
+
+        // get all the other resources
+        File[] files = folder.listFiles(new FilenameFilter() {
+
+            public boolean accept(File dir, String name) {
+                System.out.println(name);
+                return name.endsWith("wsdl");
+            }
+        });
+
         BPELDeployerClient client = new BPELDeployerClient(address);
         boolean result = false;
         try {
-            result = client.deploy(bpel, null);
+            result = client.deploy(bpel, files);
         } catch (DSBWebServiceException e) {
             e.printStackTrace();
         }
-        
+
         System.out.println(result);
-        
+
         System.exit(0);
 
     }
