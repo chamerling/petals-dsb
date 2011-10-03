534a689251ee68f1a0909d3d289a381a18bd06f7
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-10-03 14:58:21 +0000

    easyResource implementation
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@28811 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-kernel-resourcesservice/src/main/java/org/petalslink/dsb/kernel/resources/service/ExecutionEnvironmentManagerService.java
@@ -0,0 +1,304 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.kernel.resources.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.objectweb.fractal.fraclet.annotation.annotations.FractalComponent;
+import org.objectweb.fractal.fraclet.annotation.annotations.Interface;
+import org.objectweb.fractal.fraclet.annotation.annotations.LifeCycle;
+import org.objectweb.fractal.fraclet.annotation.annotations.Monolog;
+import org.objectweb.fractal.fraclet.annotation.annotations.Provides;
+import org.objectweb.fractal.fraclet.annotation.annotations.Requires;
+import org.objectweb.fractal.fraclet.annotation.annotations.type.LifeCycleType;
+import org.objectweb.util.monolog.api.Logger;
+import org.ow2.petals.jbi.messaging.registry.EndpointRegistry;
+import org.ow2.petals.jbi.messaging.registry.RegistryException;
+import org.ow2.petals.kernel.configuration.ConfigurationService;
+import org.ow2.petals.util.LoggingUtil;
+import org.ow2.petals.util.XMLUtil;
+import org.petalslink.dsb.api.ServiceEndpoint;
+import org.petalslink.dsb.jbi.Adapter;
+import org.petalslink.dsb.kernel.api.DSBConfigurationService;
+import org.w3c.dom.Document;
+
+import com.petalslink.easyresources.execution_environment_connection_api._1.GetAdditionalContent;
+import com.petalslink.easyresources.execution_environment_connection_api._1.GetAdditionalContentResponse;
+import com.petalslink.easyresources.execution_environment_connection_api._1.GetContent;
+import com.petalslink.easyresources.execution_environment_connection_api._1.GetContentResponse;
+import com.petalslink.easyresources.execution_environment_connection_api._1.GetResourceIdentifiers;
+import com.petalslink.easyresources.execution_environment_connection_api._1.GetResourceIdentifiersResponse;
+import com.petalslink.easyresources.execution_environment_connection_api._1_0.ExecutionEnvironmentManager;
+import com.petalslink.easyresources.execution_environment_connection_api._1_0.GetAdditionalContentFault;
+import com.petalslink.easyresources.execution_environment_connection_api._1_0.GetContentFault;
+import com.petalslink.easyresources.execution_environment_connection_api._1_0.GetExecutionEnvironmentInformationFault;
+import com.petalslink.easyresources.execution_environment_connection_api._1_0.GetResourceIdentifiersFault;
+import com.petalslink.easyresources.execution_environment_connection_model.ExecutionEnvironmentInformationType;
+import com.petalslink.easyresources.execution_environment_connection_model.ExecutionEnvironmentInformationTypeType;
+import com.petalslink.easyresources.execution_environment_connection_model.InterfaceConnector;
+import com.petalslink.easyresources.execution_environment_connection_model.ResourceIdentifier;
+
+/**
+ * @author chamerling
+ * 
+ */
+@FractalComponent
+@Provides(interfaces = { @Interface(name = "service", signature = ExecutionEnvironmentManager.class) })
+public class ExecutionEnvironmentManagerService implements ExecutionEnvironmentManager {
+
+    @Requires(name = "endpoint", signature = EndpointRegistry.class)
+    private EndpointRegistry endpointRegistry;
+
+    @Requires(name = "configuration", signature = ConfigurationService.class)
+    private ConfigurationService configurationService;
+
+    @Requires(name = "dsbconfiguration", signature = DSBConfigurationService.class)
+    private DSBConfigurationService dsbConfigurationService;
+
+    @Monolog(name = "logger")
+    private Logger logger;
+
+    private LoggingUtil log;
+
+    @LifeCycle(on = LifeCycleType.START)
+    protected void start() {
+        this.log = new LoggingUtil(this.logger);
+    }
+
+    @LifeCycle(on = LifeCycleType.STOP)
+    protected void stop() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.petalslink.easyresources.execution_environment_connection_api._1_0
+     * .ExecutionEnvironmentManager
+     * #getAdditionalContent(com.petalslink.easyresources
+     * .execution_environment_connection_api._1.GetAdditionalContent)
+     */
+    public GetAdditionalContentResponse getAdditionalContent(
+            GetAdditionalContent getAdditionalContent) throws GetAdditionalContentFault {
+        if (log.isInfoEnabled()) {
+            log.info("Got a getAdditionalContent call");
+        }
+        GetAdditionalContentResponse response = new GetAdditionalContentResponse();
+
+        ServiceEndpoint se = getEndpoint(getAdditionalContent.getResourceIdentifier().getId());
+        if (se == null) {
+            throw new GetAdditionalContentFault(String.format(
+                    "Impossible to find endpoint corresponding to this qname: %s",
+                    getAdditionalContent.getResourceIdentifier().getId()));
+        }
+
+        // get the WSDL import according to the input path
+        // FIXME : the ESB/DSB API does not have this feature...
+        String relativePath = getAdditionalContent.getId();
+
+        Document doc = getDescription(se.getDescription());
+
+        Document importDesc = getImport(doc, relativePath);
+        if (importDesc == null) {
+            throw new GetAdditionalContentFault(String.format(
+                    "Impossible to find ressource corresponding to %s on endpoint %s",
+                    relativePath, getAdditionalContent.getResourceIdentifier().getId()));
+        }
+        response.setAny(importDesc.getDocumentElement());
+        return response;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.petalslink.easyresources.execution_environment_connection_api._1_0
+     * .ExecutionEnvironmentManager
+     * #getResourceIdentifiers(com.petalslink.easyresources
+     * .execution_environment_connection_api._1.GetResourceIdentifiers)
+     */
+    public GetResourceIdentifiersResponse getResourceIdentifiers(
+            GetResourceIdentifiers getResourceIdentifiers) throws GetResourceIdentifiersFault {
+        if (log.isInfoEnabled()) {
+            log.info("Got a getResourceIdentifiers call");
+        }
+
+        GetResourceIdentifiersResponse response = new GetResourceIdentifiersResponse();
+
+        // get all the endpoints
+        List<ServiceEndpoint> endpoints = null;
+        try {
+            endpoints = getEndpoints();
+        } catch (RegistryException e) {
+            e.printStackTrace();
+            throw new GetResourceIdentifiersFault(
+                    "Error while trying to get endpoints from infrastructure");
+        }
+        for (ServiceEndpoint serviceEndpoint : endpoints) {
+            ResourceIdentifier epInfo = new ResourceIdentifier();
+            // FIXME : is it enough? Maybe add location...
+            epInfo.setId(serviceEndpoint.getEndpointName());
+            epInfo.setResourceType("endpoint");
+            response.getResourceIdentifier().add(epInfo);
+        }
+        return response;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.petalslink.easyresources.execution_environment_connection_api._1_0
+     * .ExecutionEnvironmentManager
+     * #getContent(com.petalslink.easyresources.execution_environment_connection_api
+     * ._1.GetContent)
+     */
+    public GetContentResponse getContent(GetContent getContent) throws GetContentFault {
+        if (log.isInfoEnabled()) {
+            log.info("Got a getContent call");
+        }
+
+        GetContentResponse response = new GetContentResponse();
+
+        // only get the WSDLs for now...
+
+        // get the endpoint from its ID
+        ServiceEndpoint se = getEndpoint(getContent.getResourceIdentifier().getId());
+        if (se == null) {
+            throw new GetContentFault(String.format(
+                    "Impossible to find endpoint corresponding to this qname: %s", getContent
+                            .getResourceIdentifier().getId()));
+        }
+
+        String description = se.getDescription();
+        if (description == null) {
+            throw new GetContentFault(String.format(
+                    "No service description found for endpoint: %s", getContent
+                            .getResourceIdentifier().getId()));
+        }
+
+        Document doc = getDescription(se.getDescription());
+        response.setAny(doc.getDocumentElement());
+        return response;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.petalslink.easyresources.execution_environment_connection_api._1_0
+     * .ExecutionEnvironmentManager#getExecutionEnvironmentInformation()
+     */
+    public ExecutionEnvironmentInformationType getExecutionEnvironmentInformation()
+            throws GetExecutionEnvironmentInformationFault {
+        if (log.isInfoEnabled()) {
+            log.info("Got a getExecutionEnvironmentInformation call");
+        }
+
+        ExecutionEnvironmentInformationType result = new ExecutionEnvironmentInformationType();
+
+        // Get local node information
+        String nodeName = configurationService.getContainerConfiguration().getName();
+        String nodeVersion = "1.0-SNAPSHOT";
+        result.setName(nodeName);
+        result.setType(ExecutionEnvironmentInformationTypeType.ESB);
+        result.setVersion(nodeVersion);
+
+        // interface connectors
+        com.petalslink.easyresources.execution_environment_connection_model.ObjectFactory objFactory = new com.petalslink.easyresources.execution_environment_connection_model.ObjectFactory();
+
+        InterfaceConnector itfSubscribeProducer = new InterfaceConnector();
+        itfSubscribeProducer.setId(objFactory
+                .createInterfaceConnectorId("resourcesSubscriptionEndpoint"));
+        itfSubscribeProducer.setInterfaceName(new QName(
+                "http://www.petalslink.com/wsn/service/WsnProducer", "NotificationProducer"));
+        itfSubscribeProducer.setEndpointAddress(getNotificationEndpoint());
+        result.getInterfaceConnector().add(itfSubscribeProducer);
+
+        InterfaceConnector itfUnSubscribeProducer = new InterfaceConnector();
+        itfUnSubscribeProducer.setId(objFactory
+                .createInterfaceConnectorId("resourcesUnSubscriptionEndpoint"));
+        itfUnSubscribeProducer
+                .setInterfaceName(new QName("http://www.petalslink.com/wsn/service/WsnProducer",
+                        "PausableSubscriptionManager"));
+        itfUnSubscribeProducer.setEndpointAddress(getProducerEndpoint());
+        result.getInterfaceConnector().add(itfUnSubscribeProducer);
+
+        return result;
+    }
+
+    /**
+     * @param description
+     * @return
+     */
+    private Document getDescription(String description) {
+        return XMLUtil.createDocumentFromString(description);
+    }
+
+    /**
+     * @param doc
+     * @param relativePath
+     * @return
+     */
+    private Document getImport(Document doc, String relativePath) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * @param id
+     * @return
+     */
+    private ServiceEndpoint getEndpoint(String id) {
+        ServiceEndpoint result = null;
+        List<org.ow2.petals.jbi.messaging.endpoint.ServiceEndpoint> endpoints;
+        try {
+            endpoints = endpointRegistry.getEndpoints();
+        } catch (RegistryException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        for (org.ow2.petals.jbi.messaging.endpoint.ServiceEndpoint serviceEndpoint : endpoints) {
+            if (serviceEndpoint.getEndpointName().equals(id)) {
+                return Adapter.createServiceEndpoint(serviceEndpoint);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @return
+     */
+    private String getProducerEndpoint() {
+        return dsbConfigurationService.getWSKernelBaseURL() + "NotificationProducer";
+    }
+
+    /**
+     * @return
+     */
+    private String getNotificationEndpoint() {
+        return dsbConfigurationService.getWSKernelBaseURL() + "NotificationConsumer";
+    }
+
+    /**
+     * @return
+     * @throws RegistryException
+     */
+    private List<ServiceEndpoint> getEndpoints() throws RegistryException {
+        final List<ServiceEndpoint> result = new ArrayList<ServiceEndpoint>();
+        List<org.ow2.petals.jbi.messaging.endpoint.ServiceEndpoint> endpoints = this.endpointRegistry
+                .getEndpoints();
+
+        for (org.ow2.petals.jbi.messaging.endpoint.ServiceEndpoint serviceEndpoint : endpoints) {
+            ServiceEndpoint se = Adapter.createServiceEndpoint(serviceEndpoint);
+            result.add(se);
+        }
+
+        return result;
+    }
+}
