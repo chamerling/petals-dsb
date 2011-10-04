05cc6566366360be68efbd0263c8c4cdb46dcb9c
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-10-04 07:57:16 +0000

    A registry listener sample
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@28824 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-kernel/src/main/java/org/petalslink/dsb/kernel/registry/sample/RegistryListenerSample.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.kernel.registry.sample;
+
+import org.objectweb.fractal.fraclet.annotation.annotations.FractalComponent;
+import org.objectweb.fractal.fraclet.annotation.annotations.Interface;
+import org.objectweb.fractal.fraclet.annotation.annotations.LifeCycle;
+import org.objectweb.fractal.fraclet.annotation.annotations.Monolog;
+import org.objectweb.fractal.fraclet.annotation.annotations.Provides;
+import org.objectweb.fractal.fraclet.annotation.annotations.type.LifeCycleType;
+import org.objectweb.util.monolog.api.Logger;
+import org.ow2.petals.util.LoggingUtil;
+import org.petalslink.dsb.api.DSBException;
+import org.petalslink.dsb.api.ServiceEndpoint;
+import org.petalslink.dsb.kernel.api.messaging.RegistryListener;
+
+/**
+ * A registry listener sample, just add the registry listener annotation and it
+ * will be detected by the DSB at startup.
+ * 
+ * @author chamerling
+ * 
+ */
+@org.petalslink.dsb.annotations.registry.RegistryListener
+@FractalComponent
+@Provides(interfaces = { @Interface(name = "service", signature = RegistryListener.class) })
+public class RegistryListenerSample implements RegistryListener {
+
+    @Monolog(name = "logger")
+    private Logger logger;
+
+    private LoggingUtil log;
+
+    @LifeCycle(on = LifeCycleType.START)
+    protected void start() {
+        this.log = new LoggingUtil(this.logger);
+    }
+
+    @LifeCycle(on = LifeCycleType.STOP)
+    protected void stop() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.petalslink.dsb.kernel.api.messaging.RegistryListener#onRegister(org
+     * .petalslink.dsb.api.ServiceEndpoint)
+     */
+    public void onRegister(ServiceEndpoint endpoint) throws DSBException {
+        if (log.isDebugEnabled()) {
+            log.debug("A new endpoint has been registered : " + endpoint);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.petalslink.dsb.kernel.api.messaging.RegistryListener#onUnregister
+     * (org.petalslink.dsb.api.ServiceEndpoint)
+     */
+    public void onUnregister(ServiceEndpoint endpoint) throws DSBException {
+        if (log.isDebugEnabled()) {
+            log.debug("An endpoint has been unregistered : " + endpoint);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.petalslink.dsb.kernel.api.messaging.RegistryListener#getName()
+     */
+    public String getName() {
+        return "RegistryListenerSample";
+    }
+
+}
