6706c16998e55a416ffa8c85ee59ea8b1e44a561
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-10-06 09:17:31 +0000

    add test
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@28918 7b29199d-e5f1-46e2-806f-aa95afc057b2

--- modules/petals-generator-bpel/src/test/java/org/petalslink/dsb/tools/generator/bpel/BPELGeneratorTest.java
+++ modules/petals-generator-bpel/src/test/java/org/petalslink/dsb/tools/generator/bpel/BPELGeneratorTest.java
@@ -42,5 +42,29 @@ public class BPELGeneratorTest extends TestCase {
         //FileUtils.deleteDirectory(input);
 
     }
+    
+    public void testGenerateFactorial() throws Exception {
+        URL url = UtilsTest.class.getResource("/factorial");
+        File inputFolder = new File(url.toURI());
+        System.out.println(inputFolder);
+        File f = new File("test");
+        File input = new File(f, UUID.randomUUID().toString());
+        System.out.println(input);
+        
+        // copy input files
+        FileUtils.copyDirectory(inputFolder, input, true);
+        System.out.println("Let's work from : " + input.getAbsolutePath());
+        File output = new File(input, "output");
+        
+        Map<String, String> extensions = new HashMap<String, String>();
+        
+        BPELGenerator gen = new BPELGenerator(inputFolder, output, "1.0", extensions);
+        File out = gen.generate();
+        System.out.println("Output generated in " + out);
+        
+        
+        //FileUtils.deleteDirectory(input);
+
+    }
 
 }
