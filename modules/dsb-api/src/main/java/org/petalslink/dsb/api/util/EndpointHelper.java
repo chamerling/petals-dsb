e30c39ac8ce51af314e51b308a60f3c06c57b78e
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-10-10 16:31:27 +0000

    Endpoint representation helper for URIs
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@29010 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-api/src/main/java/org/petalslink/dsb/api/util/EndpointHelper.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.api.util;
+
+import java.net.URI;
+
+import javax.xml.namespace.QName;
+
+import org.petalslink.dsb.api.ServiceEndpoint;
+
+/**
+ * @author chamerling
+ * 
+ */
+public class EndpointHelper {
+
+    static final String DSB_PREFIX = "dsb";
+
+    /**
+     * 
+     */
+    private EndpointHelper() {
+    }
+
+    public static boolean isDSBService(String uri) {
+        return uri != null && uri.startsWith(DSB_PREFIX + "://");
+    }
+
+    public static boolean isDSBService(URI uri) {
+        return uri != null && uri.toString().startsWith(DSB_PREFIX + "://");
+    }
+
+    public static org.petalslink.dsb.api.ServiceEndpoint getServiceEndpoint(URI uri) {
+        ServiceEndpoint result = null;
+        if (uri == null || !isDSBService(uri)) {
+            return null;
+        }
+        result = new ServiceEndpoint();
+        result.setEndpointName(getEndpoint(uri));
+        result.setServiceName(getService(uri));
+        return result;
+
+    }
+
+    public static QName getService(URI uri) {
+        if (uri == null || !isDSBService(uri)) {
+            return null;
+        }
+
+        String tmp = uri.toString();
+        String serviceName = null;
+        if (tmp.contains("@")) {
+            serviceName = tmp.substring((DSB_PREFIX + "://").length(), tmp.indexOf('@'));
+        } else {
+            // we do not support URIs where service and endpoint are not both
+            // defined
+            return null;
+        }
+
+        String ns = "http://" + serviceName.substring(0, serviceName.lastIndexOf('/') + 1);
+        String localPart = serviceName.substring(serviceName.lastIndexOf('/') + 1);
+        return new QName(ns, localPart);
+    }
+
+    public static String getEndpoint(URI uri) {
+        if (uri == null || !isDSBService(uri)) {
+            return null;
+        }
+
+        String tmp = uri.toString();
+        String endpointName = null;
+        if (tmp.contains("@")) {
+            endpointName = tmp.substring(tmp.indexOf('@') + 1);
+        } else {
+            // we do not support URIs where service and endpoint are not both
+            // defined
+            return null;
+        }
+        return endpointName;
+    }
+}
