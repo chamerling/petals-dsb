db0c477a06494cfab1ac3a5f66ed66944f25a816
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-10-10 16:56:52 +0000

    COMMONS-75 KERNEL may be able to subscribe to itself on behalf of subscribers
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@29012 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-kernel-pubsubservice/src/main/java/org/petalslink/dsb/kernel/pubsub/service/SubscriberBootstrapImpl.java
@@ -0,0 +1,106 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.kernel.pubsub.service;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+import java.util.Properties;
+
+import org.objectweb.fractal.fraclet.annotation.annotations.FractalComponent;
+import org.objectweb.fractal.fraclet.annotation.annotations.LifeCycle;
+import org.objectweb.fractal.fraclet.annotation.annotations.Monolog;
+import org.objectweb.fractal.fraclet.annotation.annotations.type.LifeCycleType;
+import org.objectweb.util.monolog.api.Logger;
+import org.ow2.petals.util.LoggingUtil;
+import org.ow2.petals.util.XMLUtil;
+import org.petalslink.dsb.annotations.LifeCycleListener;
+import org.petalslink.dsb.annotations.Phase;
+import org.petalslink.dsb.notification.commons.PropertiesConfigurationProducer;
+import org.petalslink.dsb.notification.commons.api.ConfigurationProducer;
+import org.petalslink.dsb.notification.commons.api.NotificationManager;
+import org.w3c.dom.Document;
+
+import com.ebmwebsourcing.wsstar.basenotification.datatypes.api.abstraction.Subscribe;
+import com.ebmwebsourcing.wsstar.wsnb.services.impl.engines.NotificationProducerEngine;
+import com.ebmwebsourcing.wsstar.wsnb.services.impl.util.Wsnb4ServUtils;
+
+/**
+ * Subscribe to the local engine on behalf of others... Defined in local
+ * properties file.
+ * 
+ * @author chamerling
+ * 
+ */
+@FractalComponent
+public class SubscriberBootstrapImpl {
+
+    @Monolog(name = "logger")
+    private Logger logger;
+
+    private LoggingUtil log;
+
+    @LifeCycle(on = LifeCycleType.START)
+    protected void start() {
+        this.log = new LoggingUtil(this.logger);
+    }
+
+    @LifeCycle(on = LifeCycleType.STOP)
+    protected void stop() {
+    }
+
+    @LifeCycleListener(phase = Phase.START, priority = 0)
+    public void subscribeOnBehalf() {
+        // Let see if the notification center is available...
+        NotificationManager manager = NotificationCenter.get().getManager();
+        if (manager == null) {
+            log.warning("Can not find the notification manager");
+            return;
+        }
+
+        NotificationProducerEngine engine = manager.getNotificationProducerEngine();
+        if (engine == null) {
+            log.warning("Can not find the notification producer engine");
+            return;
+        }
+
+        // create default subscribers, ie automatically subscribe to myself for
+        // others...
+        // look if we have some configuration about subscribers...
+        URL subscribers = SubscriberBootstrapImpl.class.getClassLoader().getResource(
+                "subscribers.cfg");
+
+        Properties subscriberProps = null;
+        if (subscribers != null) {
+            subscriberProps = new Properties();
+            try {
+                subscriberProps.load(SubscriberBootstrapImpl.class.getClassLoader()
+                        .getResourceAsStream("subscribers.cfg"));
+            } catch (IOException e) {
+                log.warning("Error while loading subscribers", e);
+            }
+        }
+
+        if (subscriberProps != null) {
+            ConfigurationProducer producers = new PropertiesConfigurationProducer(subscriberProps);
+            Map<String, Subscribe> toSubscribe = producers.getSubscribe();
+            for (String key : toSubscribe.keySet()) {
+                // let's subscribe...
+                try {
+                    final com.ebmwebsourcing.wsstar.basenotification.datatypes.api.abstraction.SubscribeResponse subscribeResponse = engine
+                            .subscribe(toSubscribe.get(key));
+
+                    if (log.isDebugEnabled()) {
+                        Document doc = Wsnb4ServUtils.getWsnbWriter().writeSubscribeResponseAsDOM(
+                                subscribeResponse);
+                        log.debug("KERNEL SUBSCRIBE RESPONSE = "
+                                + XMLUtil.createStringFromDOMDocument(doc));
+                    }
+                } catch (Exception e) {
+                    log.warning("Error while subscribing", e);
+                }
+            }
+        }
+    }
+}
