161f43d1a961ba41cf34a9657b4f61f94b00ff9d
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-10-14 14:39:34 +0000

    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@29092 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-kernel-api/src/main/java/org/petalslink/dsb/kernel/api/notification/NotificationCenter.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.petalslink.dsb.kernel.api.notification;
+
+/**
+ * @author chamerling
+ *
+ */
+public interface NotificationCenter {
+    
+    /**
+     * Get the attached notification sender
+     * 
+     * @return
+     */
+    NotificationSender getNotificationSender();
+
+}
