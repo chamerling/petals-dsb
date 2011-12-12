c902aaf23f9bc2ab11bc1270bb915cfda9366f87
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-12-12 15:21:36 +0000

    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@29897 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-logger-jbise/src/main/java/org/petalslink/dsb/component/logger/JBIListener.java
@@ -0,0 +1,44 @@
+package org.petalslink.dsb.component.logger;
+
+import javax.jbi.messaging.MessagingException;
+import javax.xml.transform.TransformerException;
+
+import org.ow2.petals.component.framework.api.message.Exchange;
+import org.ow2.petals.component.framework.listener.AbstractJBIListener;
+import org.ow2.petals.component.framework.util.XMLUtil;
+import org.w3c.dom.Document;
+
+/**
+ * 
+ * @author chamerling
+ * 
+ */
+public class JBIListener extends AbstractJBIListener {
+
+    @Override
+    public boolean onJBIMessage(Exchange exchange) {
+        StringBuffer sb = new StringBuffer(
+                "####################### LOGGER ############################");
+        sb.append("\n");
+        try {
+            Document in = exchange.getInMessageContentAsDocument(true);
+            String payload = XMLUtil.createStringFromDOMDocument(in);
+            sb.append("IN MESSAGE :");
+            sb.append("\n");
+            sb.append(payload);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+
+        sb.append("\n");
+        sb.append("OPERATION : ");
+        sb.append(exchange.getOperation());
+        sb.append("\n");
+        sb.append("###########################################################");
+        System.out.println(sb.toString());
+        return true;
+    }
+
+}
