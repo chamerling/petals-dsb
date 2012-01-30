29ad5e77c3d481e73e0d1874712d21f41edcfeda
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-01-30 14:08:41 +0000

    Cache based registry
    git-svn-id: https://svn.petalslink.com/svnroot/trunk/research/commons/dsb@30811 7b29199d-e5f1-46e2-806f-aa95afc057b2

---
+++ modules/dsb-registry/src/main/java/org/petalslink/dsb/registry/RegistryImpl.java
@@ -0,0 +1,140 @@
+/**
+ * PETALS - PETALS Cloud Platform.
+ * Copyright (c) 2012 EBM Websourcing, http://www.ebmwebsourcing.com/
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.petalslink.dsb.registry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import javax.xml.namespace.QName;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+/**
+ * @author chamerling
+ * 
+ */
+public class RegistryImpl implements Registry<String> {
+
+	/**
+	 * Key is ITF QName as String
+	 */
+	LoadingCache<String, List<String>> itfs;
+
+	LoadingCache<String, List<String>> srvItf;
+
+	LoadingCache<String, List<String>> srvEpItf;
+
+	private final RegistryEntryLoader loader;
+
+	/**
+	 * Entry Time To Live. Will be deleted from the cache after ttl seconds.
+	 */
+	private long ttl;
+
+	/**
+	 * 
+	 */
+	public RegistryImpl(final RegistryEntryLoader loader, long ttl) {
+		this.loader = loader;
+		this.ttl = ttl;
+
+		itfs = CacheBuilder.newBuilder()
+				.expireAfterAccess(this.ttl, TimeUnit.SECONDS)
+				.build(new CacheLoader<String, List<String>>() {
+
+					@Override
+					public List<String> load(String key) throws Exception {
+						return RegistryImpl.this.loader.getByInterface(key);
+					}
+				});
+
+		srvItf = CacheBuilder.newBuilder()
+				.expireAfterAccess(this.ttl, TimeUnit.SECONDS)
+				.build(new CacheLoader<String, List<String>>() {
+
+					@Override
+					public List<String> load(String key) throws Exception {
+						return RegistryImpl.this.loader
+								.getByServiceInterface(key);
+					}
+				});
+
+		srvEpItf = CacheBuilder.newBuilder()
+				.expireAfterAccess(this.ttl, TimeUnit.SECONDS)
+				.build(new CacheLoader<String, List<String>>() {
+
+					@Override
+					public List<String> load(String key) throws Exception {
+						return RegistryImpl.this.loader
+								.getByServiceInterfaceEndpoint(key);
+					}
+				});
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.petalslink.dsb.registry.Registry#get(java.lang.String,
+	 * javax.xml.namespace.QName, javax.xml.namespace.QName)
+	 */
+	@Override
+	public List<String> get(String endpoint, QName service, QName itf) {
+		String key = toKey(endpoint, service, itf);
+
+		if (endpoint != null && service != null && itf != null) {
+			try {
+				return srvEpItf.get(key);
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		} else if (service != null && itf != null) {
+			try {
+				return srvItf.get(key);
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		} else if (itf != null) {
+			try {
+				return itfs.get(key);
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+		return new ArrayList<String>();
+	}
+
+	private String toKey(String endpoint, QName service, QName itf) {
+		StringBuilder sb = new StringBuilder();
+		if (endpoint != null) {
+			sb.append(endpoint);
+		}
+		if (service != null) {
+			sb.append(service.toString());
+
+		}
+		if (itf != null) {
+			sb.append(itf.toString());
+		}
+		return sb.toString();
+	}
+
+}
---
+++ modules/dsb-registry/src/test/java/org/petalslink/dsb/resgistry/RegistryImplTest.java
@@ -0,0 +1,83 @@
+/**
+ * PETALS - PETALS Cloud Platform.
+ * Copyright (c) 2012 EBM Websourcing, http://www.ebmwebsourcing.com/
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.petalslink.dsb.resgistry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.xml.namespace.QName;
+
+import org.petalslink.dsb.registry.RegistryEntryLoader;
+import org.petalslink.dsb.registry.RegistryImpl;
+
+import junit.framework.TestCase;
+
+/**
+ * @author chamerling
+ * 
+ */
+public class RegistryImplTest extends TestCase {
+
+	public void testTTL() throws Exception {
+
+		final AtomicInteger accessCount = new AtomicInteger(0);
+
+		RegistryEntryLoader loader = new RegistryEntryLoader() {
+
+			@Override
+			public List<String> getByInterface(String key) {
+				System.out.println("Getting entries for key " + key);
+				accessCount.incrementAndGet();
+				return new ArrayList<String>();
+			}
+
+			@Override
+			public List<String> getByServiceInterface(String key) {
+				System.out.println("Getting entries for key " + key);
+				accessCount.incrementAndGet();
+				return new ArrayList<String>();
+			}
+
+			@Override
+			public List<String> getByServiceInterfaceEndpoint(String key) {
+				System.out.println("Getting entries for key " + key);
+				accessCount.incrementAndGet();
+				return new ArrayList<String>();
+			}
+
+		};
+
+		RegistryImpl registry = new RegistryImpl(loader, 5);
+		List<String> result = registry.get("foobar", QName.valueOf("foo"),
+				QName.valueOf("bar"));
+		assertTrue(accessCount.get() == 1);
+		result = registry.get("foobar", QName.valueOf("foo"),
+				QName.valueOf("bar"));
+		assertTrue(accessCount.get() == 1);
+
+		// wait mmore than 5 seconds to delete the cache
+		System.out.println("Wait that the cache is deleted...");
+		TimeUnit.SECONDS.sleep(8);
+		result = registry.get("foobar", QName.valueOf("foo"),
+				QName.valueOf("bar"));
+		assertTrue(accessCount.get() == 2);
+	}
+
+}
